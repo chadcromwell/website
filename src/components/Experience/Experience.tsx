6e479c3297a0350966365bf28f3bdfe223ceda59
Chad Cromwell <chad@housescope.ca>
2021-10-24 22:34:04 -0400

    Add horizontal line to experience and update details

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,15 +1,15 @@
 import "../../css/Experience/Experience.css";
 import React from "react";
 import JobDescription from "./JobDescription";
+import HorizontalLine from "../HorizontalLine/HorizontalLine";
 
 const Experience = () => {
   return (
     <div className={"experience"}>
       <div className={"sectionHeader"}>
-        <div className={"sectionTitle roboto-bold"}>
-          <span className={"titleNumber roboto-thin"}>02. </span>
-          Where I've Worked
-        </div>
+        <div className={"titleNumber roboto-thin"}>02. </div>
+        <div className={"sectionTitle roboto-bold"}> Where I've Worked</div>
+        <HorizontalLine />
       </div>
       <JobDescription />
     </div>
