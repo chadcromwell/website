fac9fd6319bab5119bf0345a35ce355cf4e2798c
Chad Cromwell <chad@housescope.ca>
2021-10-24 22:33:43 -0400

    Add horizontal line to portfolio

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,17 +1,17 @@
 import "../../css/Portfolio/Portfolio.css";
 import React from "react";
+import HorizontalLine from "../HorizontalLine/HorizontalLine";
 
 const Portfolio = () => {
-    return (
-        <div className={"portfolio"}>
-            <div className={"sectionHeader"}>
-                <div className={"sectionTitle roboto-bold"}>
-                    <span className={"titleNumber roboto-thin"}>03. </span>
-                    A Few Things I've Made
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className={"portfolio"}>
+      <div className={"sectionHeader"}>
+        <div className={"titleNumber roboto-thin"}>03. </div>
+        <div className={"sectionTitle roboto-bold"}>A Few Things I've Made</div>
+        <HorizontalLine />
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
