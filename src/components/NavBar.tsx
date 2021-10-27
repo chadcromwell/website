cfd4d0af5e5efe88f2e6896de504e48cdfd0ed8e
Chad Cromwell <chad@housescope.ca>
2021-10-27 09:37:16 -0400

    Handle merge conflicts

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -52,6 +52,7 @@ const NavBar = () => {
             <Navbar.Toggle
               aria-controls="responsive-navbar-nav"
               style={{ opacity: opacity, transition: "opacity 0.4s ease", minHeight: "5vh", minWidth: "5vh" }}
+            />
             <Navbar.Collapse
               id="responsive-navbar-nav"
               className="justify-content-end"
